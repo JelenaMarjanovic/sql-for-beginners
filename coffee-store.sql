5a9368e52d9e7d292bef567981ef18bfcbf373e0
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-08 07:47:46 +0100

    Deleting tables

--- coffee-store.sql
+++ coffee-store.sql
@@ -38,3 +38,22 @@ ADD COLUMN coffee_origin VARCHAR(30);
 
 ALTER TABLE products
 DROP COLUMN coffee_origin;
+
+-- Deleting Tables
+CREATE DATABASE example;
+
+USE example;
+
+CREATE TABLE test(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+    age INT
+);
+
+SELECT * FROM test;
+SHOW TABLES;
+
+DROP TABLE test;
+
+SELECT * FROM test;
+SHOW TABLES;
