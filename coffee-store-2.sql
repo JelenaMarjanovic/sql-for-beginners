10c2ac3d71712958cb8cb0d26ce98e9f965c32de
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-14 22:57:05 +0100

    Updating data in tables

--- coffee-store-2.sql
+++ coffee-store-2.sql
@@ -2,6 +2,8 @@ USE coffee_store;
 
 SELECT * from products;
 
+-- Inserting Data Into Tables
+
 INSERT INTO products (name, price, coffee_origin)
 VALUES ('Espresso', 2.50, 'Brazil');
 
@@ -14,3 +16,21 @@ VALUES ('Latte', 3.50, 'Indonesia'),
 		   ('Americano', 3.00, 'Brazil'),
        ('Flat White', 3.50, 'Indonesia'),
        ('Filter', 3.00, 'India');
+       
+-- Updating Data in Tables
+UPDATE products
+SET coffee_origin = 'Sri Lanka'
+WHERE id = 7;
+
+-- Deactivating safe updates
+SET SQL_SAFE_UPDATES = 0;
+
+-- Updating two columns in one statement
+UPDATE products
+SET price = 3.25, coffee_origin = 'Ethiopia'
+WHERE name = 'Americano';
+
+-- Updating two rows in one statement
+UPDATE products
+SET coffee_origin = 'Colombia'
+WHERE coffee_origin = 'Brazil';
