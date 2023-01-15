9ba80109101d3b447528b7b1218e3a6592abdb9f
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-15 04:34:10 +0100

    Select statement

---
+++ selecting-from-a-table.sql
@@ -0,0 +1,10 @@
+USE coffee_store;
+
+-- Retrieve all the rows from a table
+SELECT * FROM customers;
+
+-- Retrieve one column from a table
+SELECT last_name FROM customers;
+
+-- Retrieve multiple columns from a table
+SELECT last_name, phone_number FROM customers;
