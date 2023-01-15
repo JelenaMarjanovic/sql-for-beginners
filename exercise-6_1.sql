3624c4689bb66fb1d307bdbd933bc02ddf70d18b
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-15 05:14:05 +0100

    Exercise 1 - actual solution

--- exercise-6_1.sql
+++ exercise-6_1.sql
@@ -2,11 +2,19 @@ USE coffee_store;
 
 -- 1. From the customers table, select the first name and phone number
 -- of all the females who have a last name of Bluth
-
+SELECT first_name, phone_number FROM customers
+WHERE gender = 'F'
+AND last_name = 'Bluth';
 
 -- 2. From the products table, select the name of all products that
 -- have a price greater than 3.00 or a coffee origin of Sri Lanka
-
+SELECT name FROM products
+WHERE price > 3.00
+OR coffee_origin = 'Sri Lanka';
 
 -- 3. How many male customers don't have a phone number
 -- entered into the customers table?
+SELECT * FROM customers
+WHERE gender = 'M'
+AND phone_number IS NULL;
+/* 3 of them */
