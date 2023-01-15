d8bed7177dc512bcf631d758b4d7aba1ffa0aaf3
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-15 04:34:49 +0100

    Where clause (including and/or)

--- selecting-from-a-table.sql
+++ selecting-from-a-table.sql
@@ -8,3 +8,20 @@ SELECT last_name FROM customers;
 
 -- Retrieve multiple columns from a table
 SELECT last_name, phone_number FROM customers;
+
+
+SELECT * FROM products;
+
+-- Retrieve rows where some clause is fulfilled
+SELECT * FROM products
+WHERE coffee_origin = 'Colombia';
+
+-- Retrieve rows where both clauses are fulfilled
+SELECT * FROM products
+WHERE price = 3.00
+AND coffee_origin = 'Colombia';
+
+-- Retrieve rows where at least one of the clauses is fulfilled
+SELECT * FROM products
+WHERE price = 3.00
+OR coffee_origin = 'Colombia';
