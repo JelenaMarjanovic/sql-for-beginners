78be24ef36eed909bb81f0d61de57ef2778b538e
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-16 02:34:06 +0100

    Between

--- selecting-from-a-table.sql
+++ selecting-from-a-table.sql
@@ -57,3 +57,16 @@ WHERE last_name IN ('Taylor', 'Bluth', 'Armstrong');
 -- Exclude multiple values from our results
 SELECT * FROM customers
 WHERE first_name NOT IN ('Katie', 'John', 'George');
+
+-- Retrieve rows where certain data is between 2 values
+SELECT * FROM orders;
+SELECT * FROM customers;
+
+SELECT product_id, customer_id, order_time FROM orders
+WHERE order_time BETWEEN '2017-01-01' AND '2017-01-07';
+
+SELECT product_id, customer_id, order_time FROM orders
+WHERE customer_id BETWEEN 5 AND 10;
+
+SELECT * FROM customers
+WHERE last_name BETWEEN 'A' AND 'L';
