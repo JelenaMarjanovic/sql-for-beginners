1549d145db332500b4242141b2e4d042881bc51b
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-21 06:16:08 +0100

    Right join

--- selecting-from-multiple-tables.sql
+++ selecting-from-multiple-tables.sql
@@ -8,10 +8,14 @@ SELECT products.name, orders.order_time FROM orders
 INNER JOIN products ON orders.product_id = products.id;
 
 -- Shorthand version
+SELECT p.name, o.order_time FROM orders o
+JOIN products p ON o.product_id = p.id;
+
 SELECT p.name, p.price, o.order_time FROM orders o
 JOIN products p ON o.product_id = p.id
 WHERE o.product_id = 5
 ORDER BY o.order_time;
+
 
 -- Left join
 
@@ -36,5 +40,18 @@ LIMIT 10;
 -- Showing importance of the order of table 1 and 2
 SELECT o.id, c.phone_number, c.last_name, o.order_time FROM customers c
 LEFT JOIN orders o ON c.id = o.customer_id
+ORDER BY o.order_time
+LIMIT 10;
+
+
+-- Right join
+SELECT o.id, c.phone_number, c.last_name, o.order_time FROM customers c
+RIGHT JOIN orders o ON c.id = o.customer_id
+ORDER BY o.order_time
+LIMIT 10;
+
+-- Showing importance of the order of table 1 and 2
+SELECT o.id, c.phone_number, c.last_name, o.order_time FROM orders o
+RIGHT JOIN customers c ON o.customer_id = c.id
 ORDER BY o.order_time
 LIMIT 10;
