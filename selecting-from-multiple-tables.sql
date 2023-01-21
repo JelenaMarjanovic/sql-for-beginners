2a397fec1bd1542569a9ddf38a03a694f94e22fc
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-21 04:36:25 +0100

    Left join

--- selecting-from-multiple-tables.sql
+++ selecting-from-multiple-tables.sql
@@ -12,3 +12,29 @@ SELECT p.name, p.price, o.order_time FROM orders o
 JOIN products p ON o.product_id = p.id
 WHERE o.product_id = 5
 ORDER BY o.order_time;
+
+-- Left join
+
+-- Fix for the optional
+UPDATE orders
+SET customer_id = 1
+WHERE id = 1;
+
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+-- Optional, what happens when there's no matching id
+UPDATE orders
+SET customer_id = NULL
+WHERE id = 1;
+
+SELECT o.id, c.phone_number, c.last_name, o.order_time FROM orders o
+LEFT JOIN customers c ON o.customer_id = c.id
+ORDER BY o.order_time
+LIMIT 10;
+
+-- Showing importance of the order of table 1 and 2
+SELECT o.id, c.phone_number, c.last_name, o.order_time FROM customers c
+LEFT JOIN orders o ON c.id = o.customer_id
+ORDER BY o.order_time
+LIMIT 10;
