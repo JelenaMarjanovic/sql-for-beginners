ea312e35a4b102c77e5cf999a9d4895a2867071c
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-24 03:56:55 +0100

    Create films table

--- cinema-booking-system.sql
+++ cinema-booking-system.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE cinema_booking_system;
 
 USE cinema_booking_system;
+
+CREATE TABLE films (
+		id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL UNIQUE,
+    length_min INT NOT NULL
+);
+
+SHOW TABLES;
+
+SELECT * FROM films;
+DESCRIBE films;
