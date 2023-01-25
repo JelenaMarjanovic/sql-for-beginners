4fe16670f5b74e76cf385d8731b7c4492b2dbbe5
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-25 18:56:04 +0100

    Non-correlated subqueries - part 1 (WHERE clause)

--- subqueries.sql
+++ subqueries.sql
@@ -12,3 +12,13 @@ SELECT screening_id, customer_id,
 		(SELECT COUNT(seat_id) FROM reserved_seat
     WHERE booking_id = b.id)
 FROM bookings b;
+
+-- Non-correlated subqueries: Part 1 - in WHERE clause
+SELECT * FROM customers;
+SELECT * FROM bookings;
+
+SELECT first_name, last_name, email FROM customers
+WHERE id IN
+		(SELECT customer_id FROM bookings
+    WHERE screening_id = 1)
+;
