33b4d59156f5da882e87047cebdfcf36caa552b0
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-26 13:22:46 +0100

    Correlated subqueries - minor addition

--- subqueries.sql
+++ subqueries.sql
@@ -11,7 +11,8 @@ WHERE film_id IN
 SELECT screening_id, customer_id,
 		(SELECT COUNT(seat_id) FROM reserved_seat
     WHERE booking_id = b.id)
-FROM bookings b;
+FROM bookings b
+ORDER BY screening_id;
 
 -- Non-correlated subqueries: Part 1 - in WHERE clause
 SELECT * FROM customers;
