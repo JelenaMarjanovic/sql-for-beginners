86d0558ee41866be4bf76463d23ace8ace1a26f2
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-27 04:16:02 +0100

    Exercise 1 - solution

---
+++ exercise-11_1.sql
@@ -0,0 +1,20 @@
+USE cinema_booking_system;
+
+-- 1. Select the film name and length for all films
+-- with a length greater than the average film length.
+SELECT name, length_min FROM films
+WHERE length_min >
+		(SELECT AVG(length_min) FROM films);
+
+-- 2. Select the maximum number and the minimum number
+-- of screenings for a particular film.
+SELECT MAX(num_of_screenings), MIN(num_of_screenings) FROM
+		(SELECT film_id, COUNT(id) AS num_of_screenings FROM screenings
+		GROUP BY film_id) nos;
+
+-- 3. Select each film name and the number of screenings
+-- for that film.
+SELECT name,
+		(SELECT COUNT(id) FROM screenings
+    WHERE film_id = f.id) AS num_of_screenings
+FROM films f;
