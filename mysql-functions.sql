3b746d2e5455d6ba9be77e7e4c735de0020ee583
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-30 16:26:04 +0100

    Upper and lower case

--- mysql-functions.sql
+++ mysql-functions.sql
@@ -17,3 +17,9 @@ SELECT SUBSTRING(name, 1, 3) AS short_name FROM films;
 SELECT SUBSTRING(name, 5, 4) AS short_name FROM films;
 SELECT SUBSTRING(name, -2, 2) AS short_name FROM films;
 SELECT SUBSTRING(name, -6, 5) AS short_name FROM films;
+
+-- Upper and lower case
+SELECT name FROM rooms;
+
+SELECT UPPER(name) AS uppercase_name FROM rooms;
+SELECT LOWER(name) AS lowercase_name FROM rooms;
