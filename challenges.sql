e36bf5ceb29b11f12dd4359f8a911480e9544eed
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-31 05:41:24 +0100

    Challenge 3 - solution

--- challenges.sql
+++ challenges.sql
@@ -10,3 +10,12 @@ JOIN films f ON f.id = s.film_id
 GROUP BY s.film_id
 ORDER BY showings DESC
 LIMIT 1;
+
+-- 3. How many bookings did the film 'Jigsaw' have in October 2017?
+SELECT COUNT(*) AS no_bookings FROM bookings
+WHERE screening_id IN
+		(SELECT id FROM screenings
+		WHERE film_id IN
+				(SELECT id FROM films
+        WHERE name = 'Jigsaw')
+    );
