ef8915f6b27e935c88a8fc1377cd51e9abf6cd80
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-31 05:06:57 +0100

    Challenge 2 - solution

--- challenges.sql
+++ challenges.sql
@@ -3,3 +3,10 @@ USE cinema_booking_system;
 -- 1. Which films are over 2 hours long?
 SELECT name, length_min FROM films
 WHERE length_min > 120;
+
+-- 2. Which film had the most screenings in October 2017?
+SELECT f.name, COUNT(s.film_id) AS showings FROM screenings s
+JOIN films f ON f.id = s.film_id
+GROUP BY s.film_id
+ORDER BY showings DESC
+LIMIT 1;
