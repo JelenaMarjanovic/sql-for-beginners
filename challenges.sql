aec4e448a2c3ca84f3343fbebb258efc090d997e
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-31 05:56:00 +0100

    Challenge 4 - solution

--- challenges.sql
+++ challenges.sql
@@ -19,3 +19,10 @@ WHERE screening_id IN
 				(SELECT id FROM films
         WHERE name = 'Jigsaw')
     );
+
+-- 4. Which 5 customers made the most bookings in October 2017?
+SELECT c.first_name, c.last_name, COUNT(b.id) AS no_bookings FROM bookings b
+JOIN customers c ON c.id = b.customer_id
+GROUP BY c.first_name, c.last_name
+ORDER BY no_bookings DESC
+LIMIT 5;
