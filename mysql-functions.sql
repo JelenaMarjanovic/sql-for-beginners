b0cd6e09ab61c4d98794088309d3f50751f91267
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-01-31 03:09:51 +0100

    Month function

--- mysql-functions.sql
+++ mysql-functions.sql
@@ -37,3 +37,12 @@ SELECT * FROM screenings
 WHERE DATE(start_time) = '2017-10-03';
 SELECT * FROM screenings
 WHERE DATE(start_time) BETWEEN '2017-10-03' AND '2017-10-05';
+
+-- Month function
+SELECT MONTH('2018-06-05');
+SELECT MONTH('2018-06-05 07:45:32');
+
+SELECT MONTH(start_time) AS month FROM screenings;
+
+SELECT * FROM screenings
+WHERE MONTH(start_time) = '10';
